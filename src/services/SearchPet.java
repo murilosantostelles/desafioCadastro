f3344b339b2f4ad79336caa5ee4bd8409265e7cb
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-18 13:15:55 -0300

    feat: Funcionalidade de busca refinada aplicada no código

--- src/services/SearchPet.java
+++ src/services/SearchPet.java
@@ -1,21 +1,17 @@
 package services;
 
 import pet.Pet;
-import pet.PetAddress;
-import pet.PetGender;
 import pet.PetType;
-
-import javax.sound.midi.Soundbank;
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+
 
 public class SearchPet {
     Scanner input = new Scanner(System.in);
     PetStorage petStorage = new PetStorage();
-    public void iniciarBusca(){
+
+    public void iniciarBusca(){ // opcao 5 do menu principal
         System.out.println("Escolha o tipo de Pet que deseja Buscar:");
         int tipoPetEscolhido = lerBuscaCachorroOuGato();
         imprimirMenuTipoDeBusca();
@@ -25,22 +21,7 @@ public class SearchPet {
         exibirResultadosBusca(buscaRefinada(todosOsPets, tipoPetEscolhido,tipoBuscaEspecifica));
     }
 
-    public void listarTodosOsPets(){ // Exibido quando o usuário aperta 4 no menu (listar todos os pets)
-        List<Pet> todosOsPets = petStorage.carregarTodosOsPets();
-        if(todosOsPets.isEmpty()){
-            System.out.println("Não há pets cadastrados no momento.");
-            return;
-        }
-        System.out.println("-=-=-=-=-LISTA DE PETS-==-=-=-=-");
-        int contador = 1;
-        for (Pet pet : todosOsPets) {
-            System.out.println(contador+". "+pet.exibirDadosDoPetEmLinha());
-            contador ++;
-        }
-        System.out.println(" ");
-    }
-
-    private List<Pet> buscaRefinada(List<Pet> todosOsPets , int tipoPetEscolhido, int tipoBuscaEspecifica){
+    private List<Pet> buscaRefinada(List<Pet> todosOsPets , int tipoPetEscolhido, int tipoBuscaEspecifica){ //aplicação dos filtros na busca
         PetType tipoEscolhido = (tipoPetEscolhido == 1) ? PetType.CACHORRO : PetType.GATO;
         List<Pet> petsFiltrados = new ArrayList<>();
 
@@ -60,6 +41,81 @@ public class SearchPet {
                     }
                 }
                 return petsFiltradosPorNome;
+            case 2:
+                System.out.println("Digite o Sexo (M/F) para buscar: ");
+                String sexoBusca = input.nextLine();
+                List<Pet> petsFiltradosPorSexo = new ArrayList<>();
+                for (Pet pet : petsFiltrados) {
+                    if(pet.getPetGender().toString().toLowerCase().contains(sexoBusca.toLowerCase())){
+                        petsFiltradosPorSexo.add(pet);
+                    }
+                }
+                return petsFiltradosPorSexo;
+            case 3:
+                System.out.println("Digite a Idade do pet para buscar: ");
+                String idadeBusca = input.nextLine();
+                List<Pet> petsFiltradosPorIdade = new ArrayList<>();
+                for (Pet pet : petsFiltrados) {
+                    if(pet.getIdade().toLowerCase().contains(idadeBusca.toLowerCase())){
+                        petsFiltradosPorIdade.add(pet);
+                    }
+                }
+                return petsFiltradosPorIdade;
+            case 4:
+                System.out.println("Digite o Peso do pet para buscar: ");
+                String pesoBusca = input.nextLine();
+                List<Pet> petsFiltradosPorPeso = new ArrayList<>();
+                for (Pet pet : petsFiltrados) {
+                    if(pet.getPeso().toLowerCase().contains(pesoBusca.toLowerCase())){
+                        petsFiltradosPorPeso.add(pet);
+                    }
+                }
+                return petsFiltradosPorPeso;
+            case 5:
+                System.out.println("Digite a Raça do pet para buscar: ");
+                String racaBusca = input.nextLine();
+                List<Pet> petsFiltradosPorRaca = new ArrayList<>();
+                for (Pet pet : petsFiltrados) {
+                    if(pet.getRaca().toLowerCase().contains(racaBusca.toLowerCase())){
+                        petsFiltradosPorRaca.add(pet);
+                    }
+                }
+                return petsFiltradosPorRaca;
+            case 6:
+                System.out.println("Digite o Endereço do Pet para Buscar: ");
+                String enderecoBusca = input.nextLine();
+                List<Pet> petsFiltradosPorEndereco = new ArrayList<>();
+                for (Pet pet : petsFiltrados) {
+                    if(pet.getEndereco().getCidade().toLowerCase().contains(enderecoBusca) || pet.getEndereco().getRua().toLowerCase().contains(enderecoBusca) ||
+                            pet.getEndereco().getNumeroCasa().toLowerCase().contains(enderecoBusca)){
+                        petsFiltradosPorEndereco.add(pet);
+                    }
+                }
+                return petsFiltradosPorEndereco;
+            case 7:
+                System.out.println("Digite o nome para buscar: ");
+                String nomeBuscaCombinada = input.nextLine();
+                System.out.println("Agora digite a Idade do pet para buscar: ");
+                String idadeBuscaCombinada = input.nextLine();
+                List<Pet> petsFiltradosPorNomeEIdade = new ArrayList<>();
+                for (Pet pet : petsFiltrados) {
+                    if(pet.getNomeCompleto().toLowerCase().contains(nomeBuscaCombinada) && pet.getIdade().contains(idadeBuscaCombinada)){
+                        petsFiltradosPorNomeEIdade.add(pet);
+                    }
+                }
+                return petsFiltradosPorNomeEIdade;
+            case 8:
+                System.out.println("Digite a Idade do Pet para Buscar: ");
+                String idadeBuscaCOmbinada2 = input.nextLine();
+                System.out.println("Agora digite o Peso do pet para Buscar: ");
+                String pesoBuscaCombinada = input.nextLine();
+                List<Pet> petsFiltradosPorIdadeEPeso = new ArrayList<>();
+                for (Pet pet : petsFiltrados) {
+                    if(pet.getIdade().contains(idadeBuscaCOmbinada2) && pet.getPeso().contains(pesoBuscaCombinada)){
+                        petsFiltradosPorIdadeEPeso.add(pet);
+                    }
+                }
+                return petsFiltradosPorIdadeEPeso;
             default:
                 System.out.println("Opção de busca inválida");
                 return new ArrayList<>();
@@ -95,7 +151,7 @@ public class SearchPet {
             }
         }
     }
-    public int lerBuscaCachorroOuGato(){
+    public int lerBuscaCachorroOuGato(){ //verifica o tipo escolhido (se é cachorro ou gato)
         int opcao;
         while (true){
             try{
@@ -117,12 +173,27 @@ public class SearchPet {
             }
         }
     }
-    public void exibirResultadosBusca(List<Pet> todosOsPets){
+
+    public void listarTodosOsPets(){ // Exibido quando o usuário aperta 4 no menu (listar todos os pets)
+        List<Pet> todosOsPets = petStorage.carregarTodosOsPets();
+        if(todosOsPets.isEmpty()){
+            System.out.println("Não há pets cadastrados no momento.");
+            return;
+        }
+        System.out.println("-=-=-=-=-LISTA DE PETS-==-=-=-=-");
+        int contador = 1;
+        for (Pet pet : todosOsPets) {
+            System.out.println(contador+". "+pet.exibirDadosDoPetEmLinha());
+            contador ++;
+        }
+        System.out.println(" ");
+    }
+
+    public void exibirResultadosBusca(List<Pet> todosOsPets){ // exibe o resultado de foma organizada (sem ser no formato do ToString)
         int contador = 1;
         for (Pet pet : todosOsPets) {
             System.out.println(contador+". "+pet.exibirDadosDoPetEmLinha());
             contador++;
         }
-
     }
 }
